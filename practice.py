5d7e3f77fe64f129377b91c74d50d808ac5a1c3f
aeahmed-rc <a.ahmed9316@gmail.com>
2022-01-05 17:06:14 -0500

    some methods and practice

--- practice.py
+++ practice.py
@@ -38,7 +38,7 @@ for name in lst1:
   
 print(lst2)
 '''
-
+'''
 ## for loop square numers and append to new lst
 
 lst1=[3, 7, 6, 8, 9, 11, 15, 25]
@@ -46,3 +46,51 @@ lst2=[]
 for num in lst1:
     lst2.append(pow(num,2))
 print(lst2)
+'''
+'''
+list1 = ['1','2','3','4']
+
+s = "+"
+
+s = s.join(list1)
+
+print(s)
+'''
+'''
+#practice .format()
+def format(name,age):
+    str = "this is {name} he is {age} yrs!".format(name=name,age=age)
+    print(str)
+
+format("ahmed","27")
+
+'''
+'''
+def acces_dict():
+    myfamily = {
+    "child1" : {
+        "name" : "Emil",
+        "year" : 2004
+    },
+    "child2" : {
+        "name" : "Tobias",
+        "year" : 2007
+    },
+    "child3" : {
+        "name" : "Linus",
+        "year" : 2011
+    }
+    }
+    child1_name = myfamily['child1']['name']
+    child2_name = myfamily['child2']['name']
+    print(child1_name,child2_name)
+acces_dict()
+'''
+
+def test_equal_operators(name):
+    if name == "Bond":
+        print("Welcome on board 007.")
+    else:
+        print("Good morning " + name)
+
+test_equal_operators("Bond")
