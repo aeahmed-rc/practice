d0fa4f69776fd75900c0503bb639bb7f2b1f4112
aeahmed-rc <a.ahmed9316@gmail.com>
2022-01-04 22:29:57 -0500

    practicing loops

--- practice.py
+++ practice.py
@@ -4,7 +4,7 @@
 #for key , value in Names.items():
         #print(key + ':', value)
 
-
+'''
 def announce(f):
     def wrapper():
         print("1")
@@ -15,5 +15,34 @@ def announce(f):
 @announce      
 def hello():
     print("hello")
-
 hello()
+'''
+
+'''
+count = [1,2,3]
+def add_list ():
+    sum =0
+    for num in range(1,4):
+        sum +=num
+        print(num)
+    print(sum)
+    
+add_list()
+'''
+'''
+lst1=["Phil", "Oz", "Seuss", "Dre"]
+lst2=[]
+for name in lst1:
+    pre ="Dr."
+    lst2.append(f"{pre}" + name)
+  
+print(lst2)
+'''
+
+## for loop square numers and append to new lst
+
+lst1=[3, 7, 6, 8, 9, 11, 15, 25]
+lst2=[]
+for num in lst1:
+    lst2.append(pow(num,2))
+print(lst2)
