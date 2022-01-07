c08f52eb61326920c4c6ae2d85481fb4a321002f
aeahmed-rc <a.ahmed9316@gmail.com>
2022-01-07 16:37:52 -0500

    lambda practice

--- practice.py
+++ practice.py
@@ -1,28 +1,8 @@
 '''
-You are given two non-empty linked lists representing two non-negative integers. 
-The digits are stored in reverse order, and each of their nodes contains a single digit. 
-Add the two numbers and return the sum as a linked list.
-
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
+Write a lambda function that takes x as parameter and returns x+2. Then assign it to a variable named L
 '''
+i=6
 
-'''
-Input: l1 = [2,4,3], l2 = [5,6,4]
-Output: [7,0,8]
-Explanation: 342 + 465 = 807.
-'''
-'''
-take list of numbers reverse them , remove the spaces
-add them to another list that we reversed 
-return the answer in a list and reversed
-'''
+L=lambda x:x+2
 
-class Solution:
-    lst1 = [2,4,3]
-    lst2 = [5,6,4]
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        reversed_lst1 = l1.reverse()
-        print(reversed_lst1)
-
-    addTwoNumbers(lst1)
+print(L(i))
