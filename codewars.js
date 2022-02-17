ac15d673a142b5006642ab78ac7a42fbb6c6d8c2
aeahmed-rc <a.ahmed9316@gmail.com>
2022-02-17 13:04:24 -0500

    converted to hex or bin

--- codewars.js
+++ codewars.js
@@ -1,25 +1,23 @@
 console.log("hello");
-/* working with regex */
-
-// calcualte slope
-
-function getSlope(p1, p2) {
-    // Return the slope of the line through p1 and p2
-    // slope is y2-y1/x2-x1
-    // ex: [1,1] [2,2]
-    //      x1,y1 x2,y2
-    // if p1 == p2 then return null
-    // if verticle aka if rise/run = ex 2/0 if run is zero
-
-    let slope = (p2[1]-p1[1])/(p2[0]-p1[0])
-    if(p2[0] == p1[0] && p2[1] ==p1[1]){
-        return null
-    }else if(p2[0] == p1[0]){
-        return null
-    }else {
-        return slope
-    }
+/* You need to create a function, helloWorld, that will return the String Hello, World! without actually using raw strings. This includes quotes, double quotes and template strings. 
+You can, however, use the String constructor and any related functions. */
+const helloWorld = (s) => {
+    // feel free to change this into a function :)
+    let str = new String(/Hello, World!/)
+    
+   return str.substring(1,str.length-1)
+  
+  };
+// convert to binary or hex
+  function evensAndOdds(num){
+	//your code here
+  if(num % 2 == 0){
+    return num.toString(2)
+  }else{
+    return num.toString(16)
+  }
 }
-  getSlope([1,3],[2,1])
+
+
 
 
