1c08c442f6f75d1126c111c2132aba08c7c77ca1
aeahmed-rc <a.ahmed9316@gmail.com>
2022-02-18 13:39:57 -0500

    muliplicative persistence

--- codewars.js
+++ codewars.js
@@ -1,23 +1,51 @@
 console.log("hello");
-/* You need to create a function, helloWorld, that will return the String Hello, World! without actually using raw strings. This includes quotes, double quotes and template strings. 
-You can, however, use the String constructor and any related functions. */
-const helloWorld = (s) => {
-    // feel free to change this into a function :)
-    let str = new String(/Hello, World!/)
-    
-   return str.substring(1,str.length-1)
+function validateWord(s)
+{
+  // loop through put each value in map
+  // add one 
+  // if value is there add another one to it
+  //if all values are same return true
   
-  };
-// convert to binary or hex
-  function evensAndOdds(num){
-	//your code here
-  if(num % 2 == 0){
-    return num.toString(2)
-  }else{
-    return num.toString(16)
-  }
+  //Code goes here!
+  let obj = {}
+  let value = 1
+  let st = s.toLowerCase()
+  for(let i =0;i<st.length;i++){
+    if(obj[st[i]]){
+      obj[st[i]] += 1
+    }else{
+      obj[st[i]] = value
+    }
+}console.log(new Set(Object.values(obj)))
+return new Set(Object.values(obj)).size === 1
+}
+function ev(s){
+    console.log(new Set(s))
 }
 
 
+/* Given a non-negative integer, return an array / a list of the individual digits in order. */
+function digitize(n) {
+    let s = n.toString().split('').map(Number)
+    return s
+    }
 
+    function per(n) {
+      
+           num = n.toString();
+       
+           let arr =[]
+         if(num.split('').length ==1){
+           return []
+         }
+           while (num.length > 1) {
+          
+               num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+               arr.push(num)
+               
+    
+           }
+           return arr.map(Number)
+       }
+    per(6)
 
