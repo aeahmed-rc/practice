90c4a48d2696ece34707c13792e1db5cb7d77cc9
aeahmed-rc <a.ahmed9316@gmail.com>
2022-03-01 13:00:23 -0500

    finding friday the 13

--- codewars.js
+++ codewars.js
@@ -179,7 +179,17 @@ function digitize(n) {
   
         console.log(total)
         }
-        calc('ABC')
+       
+        function unluckyDays(year){
+            var count = 0;
+               for (var month=0; month<12; month++) {
+                   var d = new Date(year,month,13);
+                   if(d.getDay() == 5){
+                     count++;
+                  }
+               }
+               return count;
+           }
 
 
 
