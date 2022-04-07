e710d52238bcf44256b0c6bc5c6a73a528ae20a0
aeahmed-rc <a.ahmed9316@gmail.com>
2022-04-07 14:58:59 -0400

    pattern

--- codewars.js
+++ codewars.js
@@ -423,3 +423,10 @@ function digitize(n) {
               }
 
            
+              function pattern(n){
+                var result = '1'
+                for (var i = 2; i <= n; i++){
+                  result += '\n1' + '*'.repeat(i-1) + i
+                }
+                return result
+              }
