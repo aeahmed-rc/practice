713482e7ef2eb53789af1caa9e8ed05fb4dc0c9a
aeahmed-rc <a.ahmed9316@gmail.com>
2022-04-12 16:07:01 -0400

    add arrays get a sentence

--- codewars.js
+++ codewars.js
@@ -453,3 +453,14 @@ function digitize(n) {
                })
               return ar
               }
+
+              function arrAdder(arr) {
+                var sentence = "";
+                for(var i = 0; i < arr[0].length; i++){
+                  for(var j = 0; j < arr.length; j++){
+                    sentence+=arr[j][i];
+                  }
+                  sentence+=" ";
+                }
+                return sentence.trim();
+              }
