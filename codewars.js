cd4027b66055bfaaf11b1bf9b6d995d95582501b
aeahmed-rc <a.ahmed9316@gmail.com>
2022-06-01 19:29:55 -0400

    sumallest subarray sum

--- codewars.js
+++ codewars.js
@@ -810,3 +810,26 @@ missingNo =(n)=> {
         }
         console.log(Math.max(...arry))
         return Math.max(...arry)
+      }
+
+      function smallest_subarray_sum(s, arr) {
+        let minLength = Infinity;
+        let windowSum = 0;
+        let windowStart = 0; 
+      
+        for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+          windowSum += arr[windowEnd]; // add the next element
+          
+          // shrink the window as small as possible until the 'window_sum' is smaller than 's'
+          while (windowSum >= s) {
+            minLength = Math.min(minLength, windowEnd - windowStart + 1);
+            windowSum -= arr[windowStart]
+            windowStart += 1 // sliding the window
+            }
+          }
+      
+        if (minLength === Infinity) {
+          return 0;
+        }
+        return minLength;
+      }
