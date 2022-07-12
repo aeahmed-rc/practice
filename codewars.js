d2cb2506ffa7cc9c569c799eb14f281099a8d570
aeahmed-rc <a.ahmed9316@gmail.com>
2022-07-12 09:24:45 -0400

    distinguish string from nonstring

--- codewars.js
+++ codewars.js
@@ -992,3 +992,22 @@ missingNo =(n)=> {
       return sum
         // ...
       }
+
+
+//Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
+      function divCon(x){
+        // go through array distinguish string from none string
+          //add string values together convert to integer 
+          // subtract value got from string from non string ones
+          let nonstring =0
+          let string=0
+          for(let i =0;i<x.length;i++){
+            if(typeof x[i]==='string'){
+              string+=parseFloat(x[i])
+            }else{
+              nonstring+=x[i]
+              console.log('in',x[i])
+            }
+          }
+          return nonstring-string
+        }
