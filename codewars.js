8dc405988a7d16a566e7f688d6f4c0335c2f0242
aeahmed-rc <a.ahmed9316@gmail.com>
2022-07-18 14:12:05 -0400

    filter for whats missing

--- codewars.js
+++ codewars.js
@@ -1030,3 +1030,13 @@ function countSmileys(arr) {
   return count
 
 }
+
+function findMissingLetter(array) {
+  let first = array[0].charCodeAt(0)
+  for (let i = 1; i < array.length; i++) {
+    if (first + i !== array[i].charCodeAt(0)) {
+      return String.fromCharCode(first + i)
+    }
+  }
+  throw new Error("Invalid input")
+}
