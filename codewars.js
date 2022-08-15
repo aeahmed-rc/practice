65c1582e305797a8be352e957d96310c4b44398e
aeahmed-rc <a.ahmed9316@gmail.com>
2022-08-15 16:58:59 -0400

    get last  value in cycle and add

--- codewars.js
+++ codewars.js
@@ -1452,3 +1452,11 @@ for(let n=n1;n<=range;n++){
 
     return re;
 }
+
+function sumDigNthTerm(initval, patternl, nthterm) {
+  var sum = initval;
+  for (var i = 0; i < nthterm-1; i++) {
+    sum += patternl[i%patternl.length]   
+  }
+  return (sum.toString().split("")).reduce((a,b) => Number(a)+Number(b))
+}
